ba62b5106ada814b109299cccec43ccaf17c5da3
TgJe <51323906+tangjie133@users.noreply.github.com>
2019-10-14 15:06:55 +0800

    Update pxt.json, ultrasonic.ts

---
+++ ultrasonic.ts
@@ -0,0 +1,33 @@
+
+enum Range {
+    //% block="measure_rang_500"
+    MEASURE_RANG_500 = 0x20,
+    //% block = "measure_rang_300"
+    MEASURE_RANG_300 = 0x10,
+    //% block = "measure_rang_150"
+    MEASURE_RANG_150 = 0x00
+}
+//% weight=100  color=#0020ff  block="ultrasonic"
+namespace DFRobotUltrasonic {
+    let i2cAddr = 0x11
+    /**
+     * 设置测量距离
+     */
+    //% block="Set Mode Range|%ramge"
+    //%weight = 50
+    function SetModeRange(range: Range): void {
+        let Mode = range | 0x80
+        pins.i2cWriteNumber(i2cAddr, Mode, NumberFormat.UInt8BE)
+    }
+    /**
+     * 读距离
+     */
+    //% block="read distance"
+    function i2cReadDistance(): number {
+        pins.i2cWriteNumber(i2cAddr, 3, NumberFormat.UInt16BE)
+        let buffer = pins.i2cReadBuffer(i2cAddr, 2)
+        let distance = (buffer[0] << 8) + buffer[1]
+        return distance
+    }
+
+}
